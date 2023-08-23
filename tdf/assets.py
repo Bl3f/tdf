595eb043d6bc8127a76c83e88ec4e0f34c45548f
Christophe Blefari <christophe.blefari@gmail.com>
2023-08-23 14:42:42 +0200

    2.2 — Contracts with DagsterType types validation

--- tdf/assets.py
+++ tdf/assets.py
@@ -4,6 +4,8 @@ from dagster import DailyPartitionsDefinition, asset
 from tdf.contracts import get_contract
 from tdf.resources import PostgresResource
 
+race_contract = get_contract("race")
+
 
 @asset(
     partitions_def=DailyPartitionsDefinition(
@@ -11,14 +13,14 @@ from tdf.resources import PostgresResource
     ),
     compute_kind="pandas",
     group_name="lake",
+    dagster_type=race_contract.get_dagster_typing(),
 )
 def race(context, postgres: PostgresResource) -> pd.DataFrame:
     partition_date_str = context.asset_partition_key_for_output()
-    contract = get_contract("race")
 
     return postgres.read(
         "race",
-        columns=contract.get_column_names(),
-        partition_column=contract.partition_column,
+        columns=race_contract.get_column_names(),
+        partition_column=race_contract.partition_column,
         partition_value=partition_date_str,
     )
--- tdf/contracts.py
+++ tdf/contracts.py
@@ -2,12 +2,30 @@ from dataclasses import dataclass, field
 from typing import List, Optional
 
 import yaml
+from dagster_pandas import PandasColumn, create_dagster_pandas_dataframe_type
+
+
+def get_pandas_column_type(name, type_, **kwargs):
+    if type_ == "str":
+        return PandasColumn.string_column(name, **kwargs)
+    elif type_ == "int":
+        return PandasColumn.integer_column(name, **kwargs)
+    elif type_ == "date":
+        return PandasColumn.datetime_column(name, **kwargs)
+    elif type_ == "float":
+        return PandasColumn.float_column(name, **kwargs)
+    elif type_ == "boolean":
+        return PandasColumn.boolean_column(name, **kwargs)
+    else:
+        raise Exception("Type not found.")
 
 
 @dataclass
 class Field:
     name: str
+    type: str
     description: str
+    options: dict = field(default_factory=dict)
 
 
 @dataclass
@@ -23,6 +41,18 @@ class Dataset:
 
     def get_column_names(self):
         return [col.name for col in self.columns]
+
+    def get_pandas_schema(self):
+        return [
+            get_pandas_column_type(col.name, col.type, **col.options)
+            for col in self.columns
+        ]
+
+    def get_dagster_typing(self):
+        return create_dagster_pandas_dataframe_type(
+            name=self.name,
+            columns=self.get_pandas_schema(),
+        )
 
 
 def get_contracts():
