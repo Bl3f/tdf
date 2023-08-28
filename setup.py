8de49d55a518dbff8ed1f02d28c999f147b10d70
Christophe Blefari <christophe.blefari@gmail.com>
2023-08-28 13:16:53 +0200

    9.2 — Adding the missing packages to tdf setup.py for Dagster cloud to know about the deps

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import find_packages, setup
 setup(
     name="tdf",
     packages=find_packages(exclude=["tdf_tests"]),
-    install_requires=["dagster", "dagster-cloud"],
+    install_requires=[
+        "dagster-webserver >= 1.4.6",
+        "dagster >= 1.4.6",
+        "dagster-cloud >= 1.4.6",
+        "pandas >= 2.0.3",
+        "psycopg2-binary >= 2.9.7",
+        "dagster-pandas >= 0.20.7",
+        "dagster-gcp >= 0.20.7",
+        "gcsfs >= 2023.6.0",
+        "dagster-dbt >= 0.20.7",
+        "dbt-duckdb >= 1.6.0",
+        "bdp-contracts >= 0.1.0",
+    ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
